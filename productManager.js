dbfd0d93c0bf388c60770e9c25e4aa06dfc2d3b0
Lautaro Demonte <lauty.d.p@gmail.com>
2023-02-27 14:21:59 -0300

    agrego metodos privados para saber si el archivo con la ruta existe, leerlo, o escribirlo

--- productManager.js
+++ productManager.js
@@ -45,8 +45,37 @@ class ProductManager {
     }
   }
 
-  constructor() {
-    this.products = [];
+  #exists(archivo) {
+    /* verifico si existe el archivo */
+    try {
+      if (!fs.existsSync(archivo)) {
+        throw new Error("El archivo no existe");
+      }
+    } catch (error) {
+      console.log(`Error buscando el archivo: ${error.message}`);
+    }
+  }
+
+  async #readFile(archivo) {
+    try {
+      /* leo el archivo */
+      const data = await fs.readFileSync(archivo);
+      return JSON.parse(data);
+    } catch (error) {
+      console.log(`Error leyendo el archivo: ${error.message}`);
+    }
+  }
+
+  async #writeFile(archivo, data) {
+    try {
+      await fs.writeFileSync(archivo, JSON.stringify(data, null, 2));
+    } catch (error) {
+      console.log(`Error escribiendo el archivo: ${error.message}`);
+    }
+  }
+
+  constructor(archivo) {
+    this.archivo = archivo;
   }
 
   addProduct(product) {
