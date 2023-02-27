392fd8c2a6306922b9ba188fb610cf97585ec0d5
Lautaro Demonte <lauty.d.p@gmail.com>
2023-02-27 15:55:04 -0300

    pongo los metodos privados al final de la class

--- productManager.js
+++ productManager.js
@@ -2,81 +2,6 @@ const { checkPrimeSync } = require("crypto");
 const fs = require("fs");
 
 class ProductManager {
-  #codeGenerator(codeLength = 15) {
-    const numeros = "0123456789";
-    const letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const numYLetras = numeros + letras;
-    let code = "";
-    for (let i = 0; i < codeLength; i++) {
-      const random = Math.floor(Math.random() * numYLetras.length);
-      code += numYLetras.charAt(random);
-    }
-    return code;
-  }
-
-  #idGenerator(productsArray = []) {
-    const id =
-      productsArray.length === 0
-        ? 1
-        : productsArray[productsArray.length - 1].id + 1;
-    return id;
-  }
-
-  #paramsValidator(product) {
-    if (
-      product.title &&
-      product.description &&
-      product.price &&
-      product.thumbnail &&
-      product.stock
-    ) {
-      return true;
-    } else {
-      if (!product.title) {
-        throw new Error(`Falta el title del producto.`);
-      } else if (!product.description) {
-        throw new Error(`Falta la descripcion del producto.`);
-      } else if (!product.price) {
-        throw new Error(`Falta el precio del producto.`);
-      } else if (!product.thumbnail) {
-        throw new Error(`Falta la imagen del producto.`);
-      } else if (!product.stock) {
-        throw new Error(`Falta el stock del producto.`);
-      }
-    }
-  }
-
-  #exists(archivo) {
-    /* verifico si existe el archivo */
-    try {
-      if (!fs.existsSync(archivo)) {
-        throw new Error("El archivo no existe");
-      } else {
-        return true;
-      }
-    } catch (error) {
-      console.log(`Error buscando el archivo: ${error.message}`);
-    }
-  }
-
-  async #readFile(archivo) {
-    try {
-      /* leo el archivo */
-      const data = await fs.readFileSync(archivo);
-      return JSON.parse(data);
-    } catch (error) {
-      console.log(`Error leyendo el archivo: ${error.message}`);
-    }
-  }
-
-  async #writeFile(archivo, data) {
-    try {
-      await fs.writeFileSync(archivo, JSON.stringify(data, null, 2));
-    } catch (error) {
-      console.log(`Error escribiendo el archivo: ${error.message}`);
-    }
-  }
-
   constructor(archivo) {
     this.archivo = archivo;
   }
@@ -194,6 +119,81 @@ class ProductManager {
       console.log(
         `Error al eliminar el producto con el id solicitado: ${error.message}`
       );
+    }
+  }
+
+  #codeGenerator(codeLength = 15) {
+    const numeros = "0123456789";
+    const letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numYLetras = numeros + letras;
+    let code = "";
+    for (let i = 0; i < codeLength; i++) {
+      const random = Math.floor(Math.random() * numYLetras.length);
+      code += numYLetras.charAt(random);
+    }
+    return code;
+  }
+
+  #idGenerator(productsArray = []) {
+    const id =
+      productsArray.length === 0
+        ? 1
+        : productsArray[productsArray.length - 1].id + 1;
+    return id;
+  }
+
+  #paramsValidator(product) {
+    if (
+      product.title &&
+      product.description &&
+      product.price &&
+      product.thumbnail &&
+      product.stock
+    ) {
+      return true;
+    } else {
+      if (!product.title) {
+        throw new Error(`Falta el title del producto.`);
+      } else if (!product.description) {
+        throw new Error(`Falta la descripcion del producto.`);
+      } else if (!product.price) {
+        throw new Error(`Falta el precio del producto.`);
+      } else if (!product.thumbnail) {
+        throw new Error(`Falta la imagen del producto.`);
+      } else if (!product.stock) {
+        throw new Error(`Falta el stock del producto.`);
+      }
+    }
+  }
+
+  #exists(archivo) {
+    /* verifico si existe el archivo */
+    try {
+      if (!fs.existsSync(archivo)) {
+        throw new Error("El archivo no existe");
+      } else {
+        return true;
+      }
+    } catch (error) {
+      console.log(`Error buscando el archivo: ${error.message}`);
+    }
+  }
+
+  async #readFile(archivo) {
+    try {
+      /* leo el archivo */
+      const data = await fs.readFileSync(archivo);
+      return JSON.parse(data);
+    } catch (error) {
+      console.log(`Error leyendo el archivo: ${error.message}`);
+    }
+  }
+
+  async #writeFile(archivo, data) {
+    try {
+      await fs.writeFileSync(archivo, JSON.stringify(data, null, 2));
+    } catch (error) {
+      console.log(`Error escribiendo el archivo: ${error.message}`);
     }
   }
 }
